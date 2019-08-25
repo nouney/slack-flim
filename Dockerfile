bba0043bce75a86312ce40119c690478b4b842c0
Valentin Tjoncke <valtjo@gmail.com>
2019-08-25 20:47:15 +0200

    Dockerfile + cloud run env

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM golang:1.12
+WORKDIR /go/src/github.com/nouney/slack-flim
+ADD .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o server *.go
+
+FROM drone/ca-certs
+ARG APP_PATH
+WORKDIR /
+COPY --from=0 /go/src/github.com/nouney/slack-flim/server .
+ENTRYPOINT ["/server"]
--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-  "fmt"
-  "strings"
-  "time"
-  "net/http"
-  "encoding/json"
-  "math/rand"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 const (
-  SLACK_TOKEN = ""
+	SLACK_TOKEN = ""
 )
 
 type SlackResponse struct {
-  ResponseType  string  `json:"response_type"`
-  Text          string  `json:"text"`
-  Attachments   []SlackResponseAttachments `json:"attachments"`
+	ResponseType string                     `json:"response_type"`
+	Text         string                     `json:"text"`
+	Attachments  []SlackResponseAttachments `json:"attachments"`
 }
 
 type SlackResponseAttachments struct {
-  ImageURL      string  `json:"image_url"`
-  ThumbURL      string  `json:"thumb_url"`
-  Text          string  `json:"text"`
+	ImageURL string `json:"image_url"`
+	ThumbURL string `json:"thumb_url"`
+	Text     string `json:"text"`
 }
 
 func BaseUrl(r *http.Request) string {
@@ -38,54 +39,59 @@ func BaseUrl(r *http.Request) string {
 }
 
 func WriteJSON(w http.ResponseWriter, data interface{}) {
-  w.Header().Set("Content-Type", "application/json")
-  j, err := json.Marshal(data)
-  if err != nil {
-    fmt.Println(err)
-  }
-  w.Write(j)
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(data)
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Write(j)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  r.ParseForm()
+	r.ParseForm()
 
-  if len(r.PostForm["token"]) > 0 && r.PostForm["token"][0] != SLACK_TOKEN {
-    return
-  }
+	if len(r.PostForm["token"]) > 0 && r.PostForm["token"][0] != SLACK_TOKEN {
+		return
+	}
 
-  var idx int
-  if len(r.PostForm["text"]) > 0 {
-    selected := []int{}
-    for i, q := range DB {
-      if strings.Contains(q.Text, r.PostForm["text"][0]) {
-        selected = append(selected, i)
-      }
-    }
-    if len(selected) > 0 {
-      idx = selected[rand.Intn(len(selected))]
-    } else {
-      idx = rand.Intn(len(DB))
-    }
-  } else {
-    idx = rand.Intn(len(DB))
-  }
+	var idx int
+	if len(r.PostForm["text"]) > 0 {
+		selected := []int{}
+		for i, q := range DB {
+			if strings.Contains(q.Text, r.PostForm["text"][0]) {
+				selected = append(selected, i)
+			}
+		}
+		if len(selected) > 0 {
+			idx = selected[rand.Intn(len(selected))]
+		} else {
+			idx = rand.Intn(len(DB))
+		}
+	} else {
+		idx = rand.Intn(len(DB))
+	}
 
-  imgurl := BaseUrl(r) + "/imgs/" + DB[idx].Img
-  sr := SlackResponse {
-    ResponseType: "ephemeral",
-    Attachments: []SlackResponseAttachments{
-      {
-        ImageURL: imgurl,
-        Text: DB[idx].Text,
-      },
-    },
-  }
-  WriteJSON(w, sr)
+	imgurl := BaseUrl(r) + "/imgs/" + DB[idx].Img
+	sr := SlackResponse{
+		ResponseType: "ephemeral",
+		Attachments: []SlackResponseAttachments{
+			{
+				ImageURL: imgurl,
+				Text:     DB[idx].Text,
+			},
+		},
+	}
+	WriteJSON(w, sr)
 }
 
 func main() {
-  rand.Seed(time.Now().UnixNano())
-  http.Handle("/imgs/", http.StripPrefix("/imgs/", http.FileServer(http.Dir("./imgs/"))))
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	rand.Seed(time.Now().UnixNano())
+	http.Handle("/imgs/", http.StripPrefix("/imgs/", http.FileServer(http.Dir("./imgs/"))))
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
